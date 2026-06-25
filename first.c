fa94a35e7d690e87c7761a5e2e404873b5333941
meena <udhaya.m@thinkpalm.com>
2026-06-25 15:18:12 +0530

    second commit

--- first.c
+++ first.c
@@ -16,6 +16,11 @@ int multiply(int a, int b) {
     return a * b;
 }
 
+
+int iamnew(int a, int b) {
+    return a * b;
+}
+
 float divide(int a, int b) {
     if (b == 0) {
         printf("Error: Division by zero\n");
